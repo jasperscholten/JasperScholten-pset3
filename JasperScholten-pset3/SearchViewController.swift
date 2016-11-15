13164a50130a9bccba133c21ef9a487b5406f116
jasperscholten <j.o.scholten@student.vu.nl>
2016-11-15 15:43:46 +0100

    Homescreen with two buttons: one segues to search ViewController, one to the soon-to-be watchlist. A first version of swift code to retrieve the JSON file can be found in ViewController.swift.

---
+++ JasperScholten-pset3/SearchViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SearchViewController.swift
+//  JasperScholten-pset3
+//
+//  Created by Jasper Scholten on 15-11-16.
+//  Copyright © 2016 Jasper Scholten. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchMovie: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JasperScholten-pset3/ViewController.swift
+++ JasperScholten-pset3/ViewController.swift
@@ -13,10 +13,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // https://developer.apple.com/swift/blog/?id=37
+        // http://stackoverflow.com/questions/38292793/http-requests-in-swift-3
         
+        // bij tweede query: SO_NOAPNFALLBK failed: [42] Protocol not available, dumping backtrace
+        // enum HTTPError
         
+        let url = URL(string: "https://www.omdbapi.com/?t=star+wars&y=&plot=full&r=json")
         
+        if url == nil {
+            print("Empty string")
+        } else {
+            let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                guard let data = data else {
+                    print("Data is empty")
+                    return
+                }
+                
+                let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:AnyObject]
+                
+                if json["Error"] != nil {
+                    print(json["Error"]!)
+                } else {
+                    print(json)
+                    
+                    // DispatchQueue.main.async {
+                    //     self.label.text = json.value(forKey: "imd]bRating") as! String?
+                    // }
+                    
+                    // do it again to retrieve the poster - be smart
+                    
+                }
+            }
+            
+            task.resume()
+        }
     }
 
     override func didReceiveMemoryWarning() {
